9b5484889952f86cc68d8bc45427a3875ae1c916
branespace <branespace@users.noreply.github.com>
2015-09-18 22:50:01 -0700

    Add encryption, prep for upload, upload dialog

--- public/app/components/files/fileController.js
+++ public/app/components/files/fileController.js
@@ -1,38 +1,40 @@
 app.controller('fileController', function ($scope, fileCryptService) {
     $scope.pageName = 'File Locker';
 
-    $scope.files = [{
-        filename: "testfile",
-        size: 124,
-        tags: "test file and things"
-    },
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile", size: 124, tags: "test file and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "tesstfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile", size: 124, tags: "test file and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"},
-        {filename: "testfile2", size: 2124, tags: "test file2 and things"}];
+    $scope.files = [];
+
+    $scope.modalShown = false;
+    $scope.toggleUpload = function () {
+        $scope.upload = true;
+        $scope.modalShown = !$scope.modalShown;
+    };
 
     var enc = fileCryptService.encrypt($scope, '1234', 'pass');
-    console.log(enc.toString());
     try {
         var dec = fileCryptService.decrypt($scope, enc, 'pass');
-        console.log(dec.toString());
     } catch (e) {
         console.log(e);
         alert('stop right thar!');
         //USERALERTWARNING
     }
+
+    $scope.upload = function () {
+        var fileObj = {};
+
+        var fileInput = $('#fileinput')[0];
+        fileObj.filename = fileInput.files[0].name;
+        fileObj.size = fileInput.files[0].size;
+        fileObj.tags = $('#tags')[0].value;
+        fileObj.desc = $('#desc')[0].value;
+
+        var reader = new FileReader();
+        reader.onload = function(blob){
+            console.log($('#passphrase')[0].value);
+            var encoded = fileCryptService.encrypt($scope, blob, $('#passphrase')[0].value);
+            alert(encoded);
+        }.bind(this);
+
+        reader.readAsBinaryString(fileInput.files[0]);
+    };
 
 });
