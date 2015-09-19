96c03b44911ea179813321088bbf5c0e3945e5ff
branespace <branespace@users.noreply.github.com>
2015-09-19 12:12:39 -0700

    Finish update CRUD operations

--- public/app/components/files/fileController.js
+++ public/app/components/files/fileController.js
@@ -1,22 +1,47 @@
 app.controller('fileController', function ($scope, fileCryptService) {
     $scope.pageName = 'File Locker';
 
-    $scope.files = [];
+    $scope.files = [{name: "testfileone.txt", desc:"This is a test file", size:125, tags:"test fish water"}];
 
     $scope.modalShown = false;
+
     $scope.toggleUpload = function () {
-        $scope.upload = true;
+        $scope.mode = 'upload';
+        $scope.toggleModal();
+    };
+
+    $scope.toggleDelete = function (file) {
+        $scope.targetFile = file;
+        $scope.mode = 'delete';
+        $scope.toggleModal();
+    };
+
+    $scope.toggleUpdate = function (file) {
+        $scope.targetFile = file;
+        $scope.mode = 'update';
+        $scope.toggleModal();
+    };
+
+    $scope.toggleDownload = function (file) {
+        $scope.targetFile = file || $scope.targetFile;
+        $scope.mode = 'download';
+        $scope.toggleModal();
+    };
+
+
+    $scope.toggleModal = function () {
         $scope.modalShown = !$scope.modalShown;
     };
 
-    var enc = fileCryptService.encrypt($scope, '1234', 'pass');
-    try {
-        var dec = fileCryptService.decrypt($scope, enc, 'pass');
-    } catch (e) {
-        console.log(e);
-        alert('stop right thar!');
-        //USERALERTWARNING
-    }
+
+    /*    var enc = fileCryptService.encrypt($scope, '1234', 'pass');
+        try {
+            var dec = fileCryptService.decrypt($scope, enc, 'pass');
+        } catch (e) {
+
+            //TODO decrypt failure warning
+
+        }*/
 
     $scope.upload = function () {
         var fileObj = {};
@@ -29,12 +54,49 @@ app.controller('fileController', function ($scope, fileCryptService) {
 
         var reader = new FileReader();
         reader.onload = function(blob){
-            console.log($('#passphrase')[0].value);
-            var encoded = fileCryptService.encrypt($scope, blob, $('#passphrase')[0].value);
+            var encoded = fileCryptService.encrypt($scope, blob, $('#passphraseup')[0].value);
             alert(encoded);
         }.bind(this);
 
         reader.readAsBinaryString(fileInput.files[0]);
+
+        //TODO Send file via AJAX
+    };
+
+    $scope.download = function (file) {
+        var trigger = document.createElement('a');
+
+        //TODO get filecontents via ajax
+        try {
+            var fileContents = fileCryptService.decrypt($scope, data, $('#passphrasedn')[0].value);
+            trigger.setAttribute('href', 'data:text/plain;charset=utf-8,' + fileContents);
+            trigger.setAttribute('download', file.name);
+            if (document.createEvent) {
+                var event = document.createEvent('MouseEvents');
+                event.initEvent('click', true, true);
+                trigger.dispatchEvent(event);
+            } else {
+                trigger.click();
+            }
+        } catch (e) {
+            $scope.mode = 'wrongkey';
+        }
+    };
+
+    $scope.delete = function (file) {
+
+        //TODO send delete request
+
+    };
+
+    $scope.update = function (file) {
+        $scope.targetFile.name = $('#updName')[0].value;
+        $scope.targetFile.tags = $('#updTags')[0].value;
+        $scope.targetFile.desc = $('#updDesc')[0].value;
+        $scope.toggleModal();
+
+        //TODO send file update command
+
     };
 
 });
