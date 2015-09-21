24f51fc6aaa75e613f8f7f779d4b0b7c28c94fd1
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-21 10:12:04 -0700

    outline server

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/users_dev');
+//one db, two collections
+
+process.env.APP_SECRET = process.env.APP_SECRET || 'changemechangemechangeme';
+
+var fileRouter = require(__dirname + '/routes/files_routes');
+var userRouter = require(__dirname + '/routes/users_routes');
+
+app.use('/fl/', fileRouter);
+app.use('/fl/', userRouter);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('server is running on ' + port);
+});
