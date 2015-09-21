3fc7c78975fdbf6808a7f2293623923dc51916c6
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-21 14:20:14 -0700

    working on user_routes and adding file functions

--- routes/files_routes.js
+++ routes/files_routes.js
@@ -3,31 +3,43 @@
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var File = require(__dirname + '/../models/File');
-//passport req
+//passport req will go here
+
+var download = require(__dirname + '/../lib/download');
+var upload = require(__dirname + '/../lib/upload');
+var userFiles = require(__dirname + '/../lib/user_files');
+var updateFile = require(__dirname + '/../lib/update_file');
+var removeFile = require(__dirname + '/../lib/remove_file');
+var dataStats = require(__dirname + '/../lib/data_stats');
 
 var filesRoute = module.exports = exports = express.Router();
 
+//router points to modules that handle each function
+
 filesRoute.get('/download/:id', function(req, res) {
-  //download file
+  download(req, res);
 });
 
 filesRoute.post('/upload', function(req, res) {
-  //upload file
+  upload(req, res);
 });
 
-filesRoute.get('/allFiles', function(req, res) {
+filesRoute.get('/userFiles', function(req, res) {
   //get a list of all files for username
+  userFiles(req, res);
 });
 
 filesRoute.patch('/updateFile/:id', function(req, res) {
   //patch an existing meta data file
   //body will also contain id
+  updateFile(req, res);
 });
 
 filesRoute.delete('/removeFile/:id', function(req, res) {
-  //delete a specified file
+  removeFile(req, res);
 });
 
 filesRoute.get('/dataStats', function(req, res) {
   //return stats for entire file collection
+  dataStats(req, res);
 });
