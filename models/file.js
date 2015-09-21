e13c63f5b6b66856d7645221d7bf5543072b7ad4
branespace <branespace@users.noreply.github.com>
2015-09-21 12:57:56 -0700

    Add file model

---
+++ models/file.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var fileSchema = new mongoose.Schema({
+  metadata: {type: String, required: true},
+  fileContents: {type: String, required: true}
+});
