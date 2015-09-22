5ddc8216de247b37df6b65696ec4f13db74f4511
branespace <branespace@users.noreply.github.com>
2015-09-22 11:12:32 -0700

    Refactor filecontroller into service

--- public/app/components/files/fileController.js
+++ public/app/components/files/fileController.js
@@ -1,4 +1,4 @@
-module.exports = function ($scope, fileCryptService, fileTransferService) {
+module.exports = function ($scope, fileTransferService) {
     $scope.pageName = 'File Locker';
 
     //TODO get user file list
@@ -42,72 +42,30 @@ module.exports = function ($scope, fileCryptService, fileTransferService) {
     };
 
     $scope.upload = function () {
-        var fileObj = {};
-
-        var fileInput = $scope.fileInput;
-        fileObj.filename = fileInput.files[0].name;
-        fileObj.size = fileInput.files[0].size;
-        fileObj.tags = $scope.tags;
-        fileObj.desc = $scope.desc;
-
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            var encoded = fileCryptService.encrypt($scope, reader.result, $scope.passphraseup);
-            fileObj.fileContent = encoded;
-            var res = fileTransferService.post(fileObj);
-            res.success(function () {
-                //TODO update user file list
-                $scope.toggleModal();
-            });
-        }.bind(this);
-
-        reader.readAsArrayBuffer(fileInput.files[0]);
+        $scope.fileTransferService.post($scope, $scope.fileOperationSuccess,
+            $scope.fileOperationError);
     };
 
-    $scope.download = function () {
-
-        var res = fileTransferService.get('nourl');
-
-        res.success(function (data) {
-            var trigger = document.createElement('a');
-            try {
-                var file = {};
-                file.name = 'white';
-                var fileContents = fileCryptService.decrypt($scope, data, $scope.passphrasedn);
-                trigger.setAttribute('href', window.URL.createObjectURL(fileContents));
-                trigger.setAttribute('download', file.name);
-                if (document.createEvent) {
-                    var event = document.createEvent('MouseEvents');
-                    event.initEvent('click', true, true);
-                    trigger.dispatchEvent(event);
-                } else {
-                    trigger.click();
-                }
-                scope.toggleModal();
-            } catch (e) {
-                console.log(e);
-                $scope.mode = 'wrongkey';
-            }
-        });
-
-        res.error(function (data, status, headers, config) {
-            alert('error');
-        });
-
+    $scope.download = function (file) {
+        $scope.fileTransferService.get($scope, file, $scope.fileOperationError);
     };
 
     $scope.deleteFile = function () {
-
-        //TODO send delete request
 
     };
 
     $scope.update = function () {
 
+    };
+
+    $scope.fileOperationSuccess = function () {
         $scope.toggleModal();
+        $scope.dialogConfig = {};
+    };
 
-        //TODO send file update command
-
+    $scope.fileOperationError = function (err) {
+        $scope.toggleModal();
+        $scope.dialogConfig = {};
     };
 
 };
--- public/app/components/files/fileTransferService.js
+++ public/app/components/files/fileTransferService.js
@@ -1,12 +1,80 @@
-module.exports = function ($http) {
+module.exports = function ($http, fileCryptService) {
     var fileTransferService = {};
 
-    fileTransferService.post = function(postObj){
-        return $http.post('/', postObj);
+    fileTransferService.post = function ($scope, callback, callbackerr) {
+
+        var fileObj = {};
+        var fileInput = $scope.dialogConfig.fileInput;
+        fileObj.filename = fileInput.files[0].name;
+        fileObj.size = fileInput.files[0].size;
+        fileObj.tags = $scope.dialogConfig.tags;
+        fileObj.desc = $scope.dialogConfig.desc;
+
+        var reader = new FileReader();
+        reader.onload = function (err) {
+            fileObj.fileContent = fileCryptService.encrypt($scope,
+                reader.result, $scope.configDialog.passphraseup);
+            var res = $http.post('/', fileObj);
+            res.success(function () {
+                callback();
+            });
+            res.error(function (err) {
+                callbackerr(err)
+            });
+        }.bind(this);
+        reader.readAsArrayBuffer(fileInput.files[0]);
     };
 
-    fileTransferService.get = function(url){
-        return $http.get('/qqqq');
+    fileTransferService.get = function ($scope, file, callbackerr) {
+        var res = $http.get('/' + file.id);
+
+        res.success(function (data) {
+            var trigger = document.createElement('a');
+            try {
+                var fileContents = fileCryptService.decrypt($scope, data,
+                    $scope.configDialog.passphrasedn);
+                trigger.setAttribute('href', window.URL.createObjectURL(fileContents));
+                trigger.setAttribute('download', file.name);
+                if (document.createEvent) {
+                    var event = document.createEvent('MouseEvents');
+                    event.initEvent('click', true, true);
+                    trigger.dispatchEvent(event);
+                } else {
+                    trigger.click();
+                }
+                scope.toggleModal();
+            } catch (err) {
+                $scope.mode = 'wrongkey';
+            }
+        });
+
+        res.error(function (data) {
+            callbackerr(data);
+        });
+    };
+
+    fileTransferService.deleteFile = function ($scope, file, callback, callbackerr) {
+        var res = $http.delete('/' + file.id);
+
+        res.success(function () {
+            callback();
+        });
+
+        res.error(function (data) {
+            callbackerr(data);
+        });
+    };
+
+    fileTransferService.updateFile = function ($scope, file, callback, callbackerr) {
+        var res = $http.patch('/' + file.id);
+
+        res.success(function () {
+            callback();
+        });
+
+        res.error(function (data) {
+            callbackerr(data);
+        });
     };
 
     return fileTransferService;
