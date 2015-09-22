b45d5371c376f7d313a462ce088f1a5761b195a3
William Shull <will.shull@gmail.com>
2015-09-21 23:12:10 -0700

    created user login/signin routes in user_router. Added a token authentication route to file_router. Created the User model with create/compare hash, and generate token methods. Needs tests

--- routes/files_routes.js
+++ routes/files_routes.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var File = require(__dirname + '/../models/File');
-//passport req will go here
+var passport = require('passport');
+var bearerStrategy = require('passport-http-bearer').Strategy;
+var handleError = require(__dirname + '/../lib/handle_error');
 
 var download = require(__dirname + '/../lib/download');
 var upload = require(__dirname + '/../lib/upload');
@@ -12,34 +14,49 @@ var updateFile = require(__dirname + '/../lib/update_file');
 var removeFile = require(__dirname + '/../lib/remove_file');
 var dataStats = require(__dirname + '/../lib/data_stats');
 
+passport.use(new bearerStrategy(
+  function(token, done) {
+    User.findOne({ token: token }, function(err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      return done(null, user, {scope: 'all'});
+    });
+  }
+));
+
 var filesRoute = module.exports = exports = express.Router();
 
-//router points to modules that handle each function
+filesRoute.all('/user/*', function(req, res, next) {
+  passport.authenticate('bearer', function(err, user) {
+    if (err) throw err; //change to proper error handling
+    if (!user) throw err; //same as above
+  })(req, res, next);
+});
 
-filesRoute.get('/download/:id', function(req, res) {
+filesRoute.get('/user/download/:id', function(req, res) {
   download(req, res);
 });
 
-filesRoute.post('/upload', function(req, res) {
+filesRoute.post('/user/upload', function(req, res) {
   upload(req, res);
 });
 
-filesRoute.get('/userFiles', function(req, res) {
+filesRoute.get('/user/userFiles', function(req, res) {
   //get a list of all files for username
   userFiles(req, res);
 });
 
-filesRoute.patch('/updateFile/:id', function(req, res) {
+filesRoute.patch('/user/updateFile/:id', function(req, res) {
   //patch an existing meta data file
   //body will also contain id
   updateFile(req, res);
 });
 
-filesRoute.delete('/removeFile/:id', function(req, res) {
+filesRoute.delete('/user/removeFile/:id', function(req, res) {
   removeFile(req, res);
 });
 
-filesRoute.get('/dataStats', function(req, res) {
+filesRoute.get('/user/dataStats', function(req, res) {
   //return stats for entire file collection
   dataStats(req, res);
 });
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -4,15 +4,44 @@ var express = require('express');
 var jsonParser = require('body-parser').json();
 var File = require(__dirname + '/../models/file');
 var User = require(__dirname + '/../models/user');
-//req passport
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+
+
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username}, function(err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      user.compareHash(password, function(err, hasRes) {
+        if (err) return done(err);
+        return done(null, user);
+      });
+    });
+  }
+));
 
 var usersRoute = module.exports = exports = express.Router();
 
-usersRoute.post('/signup', function(req, res) {
-  //post for creating a new user
+usersRoute.post('/signup', jsonParser, function(req, res) {
+  var newUser = new User();
+  newUser.username = req.body.username;
+  newUser.basic.username = req.body.username;
+  newUser.basic.email = req.body.email;
+  newUser.generateHash(req.body.password, function(err, hash) {
+    if (err) throw err; //Change to proper err handler
+    newUser.save(function(err, data) {
+      if (err) throw err; //probably change this one as well
+      res.json({msg: 'account created!'});
+    });
+  });
 });
 
-usersRoute.get('/signin', function(req, res) {
-  //get route for signing with via passport
+usersRoute.get('/signin', passport.authenticate('local'), function(req, res) {
+  req.user.generateToken(function(err, token) {
+    if (err) throw err; //Stop being lazy and require in the error handler
+    req.user.token = token;
+    res.json(req.user);
+  })
 });
 
